19e4e1c48cc4bca4fd4ea5e12948008dfadb3ffa
Sagalim <sagachouch@gmx.de>
2025-04-26 22:56:16 +0200

    Added comments to code

--- Week 0/indoor.py
+++ Week 0/indoor.py
@@ -1,3 +1,5 @@
+# getting input from the user
 user_input = input("Your input: ")
 
+# print user's input in lower case
 print(user_input.lower())
--- Week 0/playback.py
+++ Week 0/playback.py
@@ -1,3 +1,5 @@
+# getting user input
 user_input = input("Your input: ")
 
+# print user's input with replaced empty space
 print(user_input.replace(' ', '...'))
