01450bd42fca3786808af7162b66c5be5f9d1b8e
Sagalim <sagachouch@gmx.de>
2025-04-29 22:11:24 +0200

    Finished bank.py

--- Week 1/bank.py
+++ Week 1/bank.py
@@ -1,1 +1,11 @@
-# not done yet
+# asking user to greet
+user_input = input("Greeting: ").lower().lstrip(" ")
+
+# if the greeting starts with 'hello'
+if user_input.startswith("hello"):
+    print("$0")
+# if the greeting starts with the letter 'h'
+elif user_input.startswith("h"):
+    print("$20")
+else:
+    print("$100")
