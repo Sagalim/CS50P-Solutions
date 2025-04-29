51314116da75222eab80f59f129ec94ab8ee853c
Sagalim <sagachouch@gmx.de>
2025-04-29 22:03:27 +0200

    Finished deep.py

--- Week 1/deep.py
+++ Week 1/deep.py
@@ -1,1 +1,9 @@
-# not done yet
+# getting user's input
+user_input = input("Your answer: ")
+
+# check the user's input using a match pattern
+match user_input.lower().replace(" ", ""):
+    case "42" | "forty-two" | "fortytwo":
+        print("Yes")
+    case _:
+        print("No")
