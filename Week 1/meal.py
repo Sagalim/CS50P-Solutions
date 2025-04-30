7517c048cf3b64514639decac7ff7a7a8c88ef4d
Sagalim <sagachouch@gmx.de>
2025-04-30 14:55:09 +0200

    Finished the meal exercise

--- Week 1/meal.py
+++ Week 1/meal.py
@@ -1,1 +1,19 @@
-# not done yet
+def main():
+    user_input = input("What time is it? ")
+    time = convert(user_input)
+
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
+
+
+def convert(time):
+    hour, minute = time.split(":")
+    return float(hour) + float(round((int(minute) / 60), 2))
+
+
+if __name__ == "__main__":
+    main()
