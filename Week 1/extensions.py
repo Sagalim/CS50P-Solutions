a860a1a0be89ba487587d1166c7f0b9a78d878be
Sagalim <sagachouch@gmx.de>
2025-04-30 12:04:55 +0200

    Finished "extension" task

--- Week 1/extensions.py
+++ Week 1/extensions.py
@@ -1,1 +1,19 @@
-# not done yet
+# asking user for file name
+user_input = input("File name: ").lower().strip()
+
+# storing the file's media type
+extension = user_input[user_input.rfind('.'):].lstrip('.')
+
+graphics = ["jpeg", "gif", "png"]
+applications = ["pdf", "zip"]
+
+if extension in graphics:
+    print(f"image/{extension}")
+elif extension == "jpg":
+    print("image/jpeg")
+elif extension in applications:
+    print(f"application/{extension}")
+elif extension == "txt":
+    print("text/plain")
+else:
+    print("application/octet-stream")
