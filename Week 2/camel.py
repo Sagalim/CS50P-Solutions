bd2b389b2de21d0e2e3a879cb76c6e589926303e
Sagalim <sagachouch@gmx.de>
2025-04-30 23:04:37 +0200

    Finished the camelCase exercise

--- Week 2/camel.py
+++ Week 2/camel.py
@@ -1,1 +1,14 @@
-# not done yet
+def main():
+    user_input = input("camelCase: ")
+    print("snake_case: " + convert(user_input))
+
+def convert(string):
+    result = ""
+    for c in string:
+        if c.islower():
+            result += c
+        elif c.isupper():
+            result += "_" + c.lower()
+    return result
+
+main()
