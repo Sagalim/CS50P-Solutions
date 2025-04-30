4846706aaed489c695ed6dfb3e78bed1d5e9bafe
Sagalim <sagachouch@gmx.de>
2025-04-30 14:28:59 +0200

    Solved the math interpreter exercise

--- Week 1/interpreter.py
+++ Week 1/interpreter.py
@@ -1,1 +1,21 @@
-# not done yet
+# storing user's mathematical expression
+user_input = input("Expression: ")
+
+# storing the components of the expression
+x, y, z = user_input.split(" ")
+
+x = float(x)
+z = float(z)
+
+if y == "+":
+    result = x + z
+    print(result)
+elif y == "-":
+    result = x - z
+    print(result)
+elif y == "*":
+    result = x * z
+    print(result)
+elif y == "/":
+    result = x / z
+    print(result)
