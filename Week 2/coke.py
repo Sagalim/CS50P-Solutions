d6d5927d58a2bcd800710065dab2fb45dc6d1227
Sagalim <sagachouch@gmx.de>
2025-05-02 23:14:21 +0200

    Finished coke exercise

--- Week 2/coke.py
+++ Week 2/coke.py
@@ -1,1 +1,13 @@
-# not done yet
+amount, paid = 50, 0
+denominations = [25, 10, 5]
+
+while paid < amount:
+    print(f"Amount Due: {amount - paid}")
+    user_input = int(input("Insert Coin: "))
+
+    if user_input in denominations:
+        paid += user_input
+    else:
+        print("Invalid coin.")
+
+print(f"Change Owed: {paid - amount}")
