22c4599d23aeccf0f7b0f97563cc9529a4ef20cc
Sagalim <sagachouch@gmx.de>
2025-05-05 21:06:50 +0200

    Finished the twttr exercise

--- Week 2/twttr.py
+++ Week 2/twttr.py
@@ -1,1 +1,14 @@
-# not done yet
+user_input = input("Input: ")
+
+
+vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+result = ""
+
+for c in user_input:
+    if c in vowels:
+        continue
+    else:
+        result += c
+
+
+print("Output: " + result)
